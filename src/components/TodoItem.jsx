6a808c6e4d0ab6848c972133e658e9acba97d7dd
hoanglong452000 <lehoanglong452000@gmail.com>
2020-01-08 22:46:16 +0700

    Long - Doi tu stateless sang stateful

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,33 +1,67 @@
-import React, { Component } from 'react'
-import classNames from 'classnames'
+// import React, { Component } from 'react'
+// import classNames from 'classnames'
 
-import './TodoItem.css'
-import { ReactComponent as Uncompleted } from '../assets/icons/uncompleted.svg'
-import { ReactComponent as Completed } from '../assets/icons/completed.svg'
+// import './TodoItem.css'
+// import { ReactComponent as Uncompleted } from '../assets/icons/uncompleted.svg'
+// import { ReactComponent as Completed } from '../assets/icons/completed.svg'
 
-class TodoItem extends Component {
-  render() {
-    const { todo, onClickItem, deleteItem } = this.props
-    return (
-      <li
-        className={classNames('list-group-item', {
-          'TodoItem-completed': todo.isCompleted
-        })}
-        style={{ color: `red` }}
-      >
-        {todo.isCompleted ? (
-          <Completed width={25} height={25} style={{ marginRight: `10px` }} />
-        ) : (
-          <Uncompleted width={25} height={25} style={{ marginRight: `10px` }} />
-        )}
-        <span onClick={onClickItem}>{todo.task} </span>
-        {todo.isCompleted && (
-          <button type='button' className='close' aria-label='Close' onClick={deleteItem}>
-            <span aria-hidden='true'>&times;</span>
-          </button>
-        )}
-      </li>
-    )
-  }
+// class TodoItem extends Component {
+//   render() {
+//     const { todo, onClickItem, deleteItem } = this.props
+//     return (
+//       <li
+//         className={classNames('list-group-item', {
+//           'TodoItem-completed': todo.isCompleted
+//         })}
+//         style={{ color: `red` }}
+//       >
+//         {todo.isCompleted ? (
+//           <Completed width={25} height={25} style={{ marginRight: `10px` }} />
+//         ) : (
+//           <Uncompleted width={25} height={25} style={{ marginRight: `10px` }} />
+//         )}
+//         <span onClick={onClickItem}>{todo.task} </span>
+//         {todo.isCompleted && (
+//           <button type='button' className='close' aria-label='Close' onClick={deleteItem}>
+//             <span aria-hidden='true'>&times;</span>
+//           </button>
+//         )}
+//       </li>
+//     )
+//   }
+// }
+// export default TodoItem
+
+import React from "react";
+import classNames from "classnames";
+import { ReactComponent as Completed } from "../assets/icons/completed.svg";
+import { ReactComponent as Uncompleted } from "../assets/icons/uncompleted.svg";
+function TodoItem(props) {
+  const { todo, onClickItem, deleteItem } = props;
+  return (
+    <li
+      className={classNames("list-group-item", {
+        "TodoItem-completed": todo.isCompleted
+      })}
+      style={{ color: `red` }}
+    >
+      {todo.isCompleted ? (
+        <Completed width={25} height={25} style={{ marginRight: `10px` }} />
+      ) : (
+        <Uncompleted width={25} height={25} style={{ marginRight: `10px` }} />
+      )}
+      <span onClick={onClickItem}>{todo.task} </span>
+      {todo.isCompleted && (
+        <button
+          type='button'
+          className='close'
+          aria-label='Close'
+          onClick={deleteItem}
+        >
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      )}
+    </li>
+  );
 }
-export default TodoItem
+export default TodoItem;
